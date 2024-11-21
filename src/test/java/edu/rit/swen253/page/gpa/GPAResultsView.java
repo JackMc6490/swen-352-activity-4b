5afb624b7fb8cbc5fc13a2e82c1dc80109fff132
Jack McCarthy <jackmc1112@gmail.com>
2024-11-20 19:47:55 -0500

    Add and Calculate tests done

--- src/test/java/edu/rit/swen253/page/gpa/GPAResultsView.java
+++ src/test/java/edu/rit/swen253/page/gpa/GPAResultsView.java
@@ -8,20 +8,30 @@ import edu.rit.swen253.utils.DomElement;
 
 public class GPAResultsView {
     
+    private DomElement viewContainer;
     private DomElement termGPA;
     private DomElement cumulativeGPA;
 
     public GPAResultsView(final DomElement viewContainer){
+        this.viewContainer = viewContainer;
+        setElements(viewContainer);
+    }
+
+    private void setElements(DomElement viewContainer){
         List<DomElement> spans = viewContainer.findChildrenBy(By.cssSelector("span.results"));
         this.termGPA = spans.get(0);
         this.cumulativeGPA = spans.get(1);
     }
 
     public String getTerm(){
+        this.termGPA.scrollIntoView();
+        setElements(viewContainer);
         return this.termGPA.getText();
     }
 
     public String getCumulative(){
+        setElements(viewContainer);
+        this.termGPA.scrollIntoView();
         return this.cumulativeGPA.getText();
     }
 }
